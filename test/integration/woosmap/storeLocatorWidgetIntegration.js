6873b7f91e818f745a76c35f03210dafab4057b4
gaelsimon <gsimon@webgeoservices.com>
2022-04-21 11:28:11 +0200

    test: Added Integration test

--- test/integration/woosmap/storeLocatorWidgetIntegration.js
+++ test/integration/woosmap/storeLocatorWidgetIntegration.js
@@ -1,16 +1,13 @@
 var assert = require('chai').assert;
-var chaiSubset = require('chai-subset');
-var chai = require('chai');
-chai.use(chaiSubset);
-
-var request = require('request');
+var cheerio = require('cheerio');
+var request = require('request-promise');
 var config = require('../it.config');
 
 describe('Store Locator Widget Integration', function () {
     this.timeout(5000);
 
     describe('StoreLocatorWidget display', function () {
-        it('should returns successful response', function (done) {
+        it('should returns successful response with non empty woosmapPublicApiKey', function (done) {
             var url = config.baseUrl + '/StoreLocatorWidget-Show';
             var myRequest = {
                 url: url,
@@ -23,6 +20,9 @@ describe('Store Locator Widget Integration', function () {
 
             request(myRequest, function (error, response) {
                 assert.equal(response.statusCode, 200, 'Expected statusCode to be 200.');
+                var $ = cheerio.load(response.body);
+                var slwConf = JSON.parse($('#slw-conf').text());
+                assert.isTrue(slwConf.woosmapPublicApiKey.startsWith('woos-'));
                 done();
             });
         });
