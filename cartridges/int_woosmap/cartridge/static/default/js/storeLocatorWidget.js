4878af2606a64b38a6c8ee41c975b629a4859d6e
gael <gsimon@webgeoservices.com>
2022-05-16 15:19:36 +0200

    chore: @description for functions

--- cartridges/int_woosmap/cartridge/static/default/js/storeLocatorWidget.js
+++ cartridges/int_woosmap/cartridge/static/default/js/storeLocatorWidget.js
@@ -128,7 +128,7 @@ module.exports = function (include) {
 
 
 /**
- * Uses woosmap store locator widget to render a map
+ * @description Uses woosmap store locator widget to render a map
  */
 function loadStoreLocatorWidget() {
     var slwConf = JSON.parse(document.getElementById('slw-conf').innerText);
