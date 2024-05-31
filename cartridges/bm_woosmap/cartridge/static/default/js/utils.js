1ced167506e0eae1f810e8ba773f93863f4b2d3d
Mathis POIGNET <mathispoignet@gmail.com>
2024-05-31 17:04:54 +0200

    Adapt config to new product line

--- cartridges/bm_woosmap/cartridge/static/default/js/utils.js
+++ cartridges/bm_woosmap/cartridge/static/default/js/utils.js
@@ -1,4 +1,22 @@
 /**
+ * @description This function parses a JSON string and converts any stringified arrays 
+ *              back into actual arrays within the resulting object to prevent wrongly 
+ *              stringified arrays.
+ * @param {string} jsonString - A JSON string that may contain stringified arrays.
+ * @returns {Object} - A JavaScript object with all stringified arrays converted to actual arrays.
+ */
+function replacer(key, value) {
+    if (typeof value === 'string' && /^\[.*\]$/.test(value)) {
+        try {
+            return JSON.parse(value);
+        } catch (e) {
+            return value; 
+        }
+    }
+    return value;
+}
+
+/**
  * @description Pretty Print JSON in all Text Areas which have the className 'json-textarea'
  */
 
@@ -6,7 +24,7 @@ function prettyPrint(elements) {
     for (var i = 0; i < elements.length; i++) {
         var ugly = elements.item(i).value;
         var obj = JSON.parse(ugly);
-        elements.item(i).value = JSON.stringify(obj, undefined, 4);
+        elements.item(i).value = JSON.stringify(obj, replacer, 4);
     }
 }
 
